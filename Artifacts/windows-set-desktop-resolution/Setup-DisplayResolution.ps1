90397bbcf88128cd222fe64472d3d119df57c3cf
JT <jakub.tomkiewicz@itmagination.com>
2017-03-09 11:49:54 +0100

    Now editing registry keys to change default resolution.

--- Artifacts/windows-set-desktop-resolution/Setup-DisplayResolution.ps1
+++ Artifacts/windows-set-desktop-resolution/Setup-DisplayResolution.ps1
@@ -1,1 +1,22 @@
-Set-DisplayResolution -Width 1600 -Height 1000 -Force
+$items = Get-ChildItem 'HKLM:\SYSTEM\CurrentControlSet\Hardware Profiles\UnitedVideo\CONTROL\VIDEO'
+$monitorItem = $false
+foreach($item in $items) {
+    $target = Get-ChildItem ($item.PSPath + '\0000')
+    if ($target) {
+        $monitorItem = $target
+        Write-Host $target
+    }
+    else {
+        Write-Host $item
+    }
+}
+
+if ($monitorItem) {
+    New-ItemProperty -Path $monitor.PSPath -Name 'DefaultSettings.XResolution' -Value 1600 -PropertyType DWORD -Force
+    New-ItemProperty -Path $monitor.PSPath -Name 'DefaultSettings.YResolution' -Value 1000 -PropertyType DWORD -Force
+}
+else {
+    Write-Host 'No monitor item found!'
+}
+
+# Set-DisplayResolution -Width 1920 -Height 1200 -Force # did not work
