5bf45114da52086c1868e00840046f980b2fa7c3
JT <jakub.tomkiewicz@itmagination.com>
2017-03-09 15:20:58 +0100

    Another try to change resolution.

--- Artifacts/windows-set-desktop-resolution/Setup-DisplayResolution.ps1
+++ Artifacts/windows-set-desktop-resolution/Setup-DisplayResolution.ps1
@@ -9,25 +9,25 @@ if (-not $Height) {
     throw "Parameter '-Height' is required."
 }
 
-# $items = Get-ChildItem 'HKLM:\SYSTEM\CurrentControlSet\Hardware Profiles\UnitedVideo\CONTROL\VIDEO'
-# $monitorItem = $false
-# foreach($item in $items) {
-#     $target = Get-ChildItem ($item.PSPath + '\0000')
-#     if ($target) {
-#         $monitorItem = $target
-#         Write-Host $target
-#     }
-#     else {
-#         Write-Host $item
-#     }
-# }
+$items = Get-ChildItem 'HKLM:\SYSTEM\CurrentControlSet\Hardware Profiles\UnitedVideo\CONTROL\VIDEO'
+$monitorItem = $false
+foreach($item in $items) {
+    $target = Get-ChildItem ($item.PSPath + '\0000')
+    if ($target) {
+        $monitorItem = $target
+        Write-Host $target
+    }
+    else {
+        Write-Host $item
+    }
+}
 
-# if ($monitorItem) {
-#     New-ItemProperty -Path $monitorItem.PSPath -Name 'DefaultSettings.XResolution' -Value $Width -PropertyType DWORD -Force
-#     New-ItemProperty -Path $monitorItem.PSPath -Name 'DefaultSettings.YResolution' -Value $Height -PropertyType DWORD -Force
-# }
-# else {
-#     Write-Host 'No monitor item found!'
-# }
+if ($monitorItem) {
+    New-ItemProperty -Path $monitorItem.PSPath -Name 'DefaultSettings.XResolution' -Value $Width -PropertyType DWORD -Force
+    New-ItemProperty -Path $monitorItem.PSPath -Name 'DefaultSettings.YResolution' -Value $Height -PropertyType DWORD -Force
+}
+else {
+    Write-Host 'No monitor item found!'
+}
 
-Set-DisplayResolution -Width $Width -Height $Height -Force
+# Set-DisplayResolution -Width $Width -Height $Height -Force # does not work
