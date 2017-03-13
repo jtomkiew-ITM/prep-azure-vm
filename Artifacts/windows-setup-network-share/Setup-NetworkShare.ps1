4be93a3ea3f32fd73cde51fba956565e927e82fa
JT <jakub.tomkiewicz@itmagination.com>
2017-03-13 15:29:50 +0100

    Remove parameter

--- Artifacts/windows-setup-network-share/Setup-NetworkShare.ps1
+++ Artifacts/windows-setup-network-share/Setup-NetworkShare.ps1
@@ -1,17 +1,5 @@
-param(
-	[string]$directory
-)
-
-if ($directory) {
-    mkdir "C:\$directory"
-}
-
 $Profile = Get-NetConnectionProfile -InterfaceAlias Ethernet*
 $profile[0].NetworkCategory = "Private"
 Set-NetConnectionProfile -InputObject $Profile
 
 netsh advfirewall firewall add rule name="File and Printer Sharing (SMB-In)" dir=in action=allow program=System profile=private localport=445 protocol=tcp
-
-# netsh firewall set service type=FILEANDPRINT mode=ENABLE profile=STANDARD # deprecated
-# netsh advfirewall firewall set rule name="File and Printer Sharing (SMB-In)" profile=private new enable=yes # might not exist
-# netsh advfirewall firewall set rule group="network discovery" new enable=yes # unnecessary
