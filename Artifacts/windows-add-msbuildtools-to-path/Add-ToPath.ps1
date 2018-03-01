69b4afc36252a8fa80964799ee39f950b15f9ff7
Tomkiewicz, Jakub (EXT) <jakub.tomkiewicz@skanska.pl>
2018-03-01 12:19:26 +0100

    New artifact: windows-add-msbuildtools-to-path

---
+++ Artifacts/windows-add-msbuildtools-to-path/Add-ToPath.ps1
@@ -0,0 +1,16 @@
+param(
+    [string] $pathToAdd = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64'
+)
+
+if (-not (Test-Path $pathToAdd)) {
+    throw "Path '$pathToAdd' does not exists"
+}
+
+$currentPath = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+
+if (-not (($currentPath -like "*$pathToAdd") -or ($currentPath -like "*$pathToAdd;*"))) {
+    [Environment]::SetEnvironmentVariable("Path", ($currentPath + ";$pathToAdd"), [System.EnvironmentVariableTarget]::Machine)
+    Write-Host "Added '$pathToAdd' to system Path"
+} else {
+    Write-Host "'$pathToAdd' already exists in system Path"
+}
