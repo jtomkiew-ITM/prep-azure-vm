9428471932a43fc50a4522cc317a4ccec337611c
Tomkiewicz, Jakub (EXT) <jakub.tomkiewicz@skanska.pl>
2018-03-02 09:26:12 +0100

    rename: windows-buildtools-fix-getreferencenearesttargetframeworktask > windows-buildtools-target-fix

--- Artifacts/windows-buildtools-target-fix/Fix-Target.ps1
+++ Artifacts/windows-buildtools-target-fix/Fix-Target.ps1
@@ -1,1 +1,2 @@
+# see: https://developercommunity.visualstudio.com/content/problem/137779/the-getreferencenearesttargetframeworktask-task-wa.html
 Start-Process "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" -ArgumentList 'modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" --quiet --add Microsoft.VisualStudio.Component.NuGet.BuildTools --add Microsoft.Net.Component.4.5.TargetingPack --norestart --force' -Wait -PassThru
